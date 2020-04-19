3533a2cade27b4a799a72d73c835d85d21c32fc0
kkeshav414 <14369793+kkeshav414@users.noreply.github.com>
2020-04-20 02:24:42 +0530

    initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Wrixby Blog`,
+    description: `An awesome blog displaying my awesome posts.`,
+    author: `Keshav Kumar`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -25,6 +25,13 @@ module.exports = {
         theme_color: `#663399`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: `k2bz2774lau9`,
+        accessToken: `6firBo1HKrBdw-pxCTnATrZ-m6-yGrSoSLnmX2EPqmA`,
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
---
+++ src/templates/blogpost.js
@@ -0,0 +1,45 @@
+import React from "react"
+import { Link, graphql } from "gatsby"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+const BlogPost = ({ data }) => {
+  const { title, content, featuredImage, tags } = data.contentfulBlogPost
+  return (
+    <Layout>
+      <SEO title={title} />
+      <div className="blogpost">
+        <h1>{title}</h1>
+        <img alt={title} src={featuredImage.file.url} />
+        <p className="body-text">{content.content}</p>
+        <div className="tags">
+          <strong>Tags: </strong>
+          {tags.map(tag => (
+            <span className="tag" key={tag}>
+              {tag}
+            </span>
+          ))}
+        </div>
+        <Link to="/blogposts">View more posts</Link>
+        <Link to="/">Back to Home</Link>
+      </div>
+    </Layout>
+  )
+}
+export default BlogPost
+export const pageQuery = graphql`
+  query($slug: String!) {
+    contentfulBlogPost(slug: { eq: $slug }) {
+      title
+      slug
+      content {
+        content
+      }
+      featuredImage {
+        file {
+          url
+        }
+      }
+      tags
+    }
+  }
+`
