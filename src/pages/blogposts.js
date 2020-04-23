e72943fd6f4937ea9a73a04c7e535e6205bb0503
kkeshav414 <14369793+kkeshav414@users.noreply.github.com>
2020-04-23 08:36:02 +0530

    blogposts error fixed

--- src/pages/blogposts.js
+++ src/pages/blogposts.js
@@ -39,6 +39,9 @@ export const query = graphql`
           content {
             content
           }
+          categories {
+            name
+          }
           featuredImage {
             file {
               url
