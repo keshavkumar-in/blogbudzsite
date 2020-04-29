941c784c0f269cd1406473100e65f847b7e550de
kkeshav414 <14369793+kkeshav414@users.noreply.github.com>
2020-04-30 05:14:40 +0530

    image transformation added

--- src/templates/blogpost.js
+++ src/templates/blogpost.js
@@ -18,22 +18,21 @@ const BlogPost = ({ pageContext, data }) => {
   } = data.contentfulBlogPost
 
   const { prev, next } = pageContext
-
   return (
     <Layout>
       <SEO title={title} />
       <article>
-        <div class="section-featured is-featured-image">
+        <div className="section-featured is-featured-image">
           <div
-            class="featured-image"
+            className="featured-image"
             style={{
               backgroundImage: `url(${featuredImage.file.url})`,
             }}
           ></div>
-          <div class="featured-wrap flex">
-            <div class="featured-content">
-              <div class="tags-wrap">
-                <span class="featured-label global-tag">
+          <div className="featured-wrap flex">
+            <div className="featured-content">
+              <div className="tags-wrap">
+                <span className="featured-label global-tag">
                   <svg
                     role="img"
                     viewBox="0 0 24 24"
@@ -44,20 +43,20 @@ const BlogPost = ({ pageContext, data }) => {
                   <span>Featured</span>
                 </span>
                 <Link
-                  class="post-tag global-tag"
+                  className="post-tag global-tag"
                   to={`/categories/${kebabCase(categories.name)}/`}
                 >
                   {categories.name}
                 </Link>
               </div>
-              <h1 class="white">{title}</h1>
-              <div class="item-meta white">
+              <h1 className="white">{title}</h1>
+              <div className="item-meta white">
                 <span>by </span>
                 <Link to={`/authors/${kebabCase(author.name)}/`}>
                   {author.name}
                 </Link>
-                , <time datetime={updatedAt}>{updatedAt}</time>
-                <span class="reading-time">
+                , <time dateTime={updatedAt}>{updatedAt}</time>
+                <span className="reading-time">
                   <svg
                     role="img"
                     viewBox="0 0 24 24"
@@ -82,8 +81,8 @@ const BlogPost = ({ pageContext, data }) => {
             </div>
           </div>
         </div>
-        <div class="section-post wrap">
-          <div class="post-wrap">
+        <div className="section-post wrap">
+          <div className="post-wrap">
             <div
               dangerouslySetInnerHTML={{
                 __html: content.childMarkdownRemark.html,
@@ -98,38 +97,38 @@ const BlogPost = ({ pageContext, data }) => {
               ))}
             </div>
           </div>
-          <div class="section-post-authors post-authors flex">
-            <div class="author-label">
+          <div className="section-post-authors post-authors flex">
+            <div className="author-label">
               <span>Author</span>
             </div>
-            <div class="author-wrap flex">
+            <div className="author-wrap flex">
               <Link
                 to={`/authors/${kebabCase(author.name)}/`}
-                class="item-link-overlay"
+                className="item-link-overlay"
               ></Link>
               <div
-                class="author-profile-image"
+                className="author-profile-image"
                 style={{
                   backgroundImage: `url(${featuredImage.file.url})`,
                 }}
               ></div>
-              <div class="author-content">
-                <h4 class="is-bio no-cover-image">
+              <div className="author-content">
+                <h4 className="is-bio no-cover-image">
                   <a href={author.name}>{author.name}</a>
                 </h4>
                 <p>{author.intro.intro}</p>
               </div>
             </div>
           </div>
-          <div class="post-meta">
-            <div class="post-share">
+          <div className="post-meta">
+            <div className="post-share">
               <a
-                class="twitter"
+                className="twitter"
                 href="https://twitter.com/intent/tweet?text=I'm%20passionate%20about%20food%2C%20the%20tradition%20of%20it%2C%20cooking%20it%2C%20sharing%20it&amp;url=https://nurui.fueko.net/im-passionate-about-food-the-tradition-of-it-cooking-it-sharing-it/"
-                onclick="window.open(this.href, 'twitter-share', 'width=550,height=235');return false;"
+                onClick={`window.open(this.href, 'twitter-share', 'width=550,height=235');return false;`}
               >
                 <svg
-                  class="global-svg"
+                  className="global-svg"
                   role="img"
                   viewBox="0 0 24 24"
                   xmlns="http://www.w3.org/2000/svg"
@@ -138,12 +137,12 @@ const BlogPost = ({ pageContext, data }) => {
                 </svg>
               </a>
               <a
-                class="facebook"
+                className="facebook"
                 href="https://www.facebook.com/sharer/sharer.php?u=https://nurui.fueko.net/im-passionate-about-food-the-tradition-of-it-cooking-it-sharing-it/"
-                onclick="window.open(this.href, 'facebook-share','width=580,height=296');return false;"
+                onClick="window.open(this.href, 'facebook-share','width=580,height=296');return false;"
               >
                 <svg
-                  class="global-svg"
+                  className="global-svg"
                   role="img"
                   viewBox="0 0 24 24"
                   xmlns="http://www.w3.org/2000/svg"
@@ -151,7 +150,7 @@ const BlogPost = ({ pageContext, data }) => {
                   <path d="M23.9981 11.9991C23.9981 5.37216 18.626 0 11.9991 0C5.37216 0 0 5.37216 0 11.9991C0 17.9882 4.38789 22.9522 10.1242 23.8524V15.4676H7.07758V11.9991H10.1242V9.35553C10.1242 6.34826 11.9156 4.68714 14.6564 4.68714C15.9692 4.68714 17.3424 4.92149 17.3424 4.92149V7.87439H15.8294C14.3388 7.87439 13.8739 8.79933 13.8739 9.74824V11.9991H17.2018L16.6698 15.4676H13.8739V23.8524C19.6103 22.9522 23.9981 17.9882 23.9981 11.9991Z" />
                 </svg>
               </a>
-              <a class="copy" id="copy" data-clipboard-target="#link-value">
+              <a className="copy" id="copy" data-clipboard-target="#link-value">
                 <svg
                   role="img"
                   viewBox="0 0 24 24"
@@ -160,18 +159,18 @@ const BlogPost = ({ pageContext, data }) => {
                   <path d="M20.33 13.04h-4.155v4.154h-2.078v-4.155H9.942v-2.077h4.155V6.806h2.078v4.156h4.154v2.077zM8.902 18.58a6.582 6.582 0 0 1 0-13.162c.068 0 .135.007.202.009a8.874 8.874 0 0 0-.001 13.144c-.067 0-.133.009-.2.009zm6.194-15.484a8.876 8.876 0 0 0-3.087.56A8.904 8.904 0 0 0 0 12a8.903 8.903 0 0 0 8.903 8.905 8.893 8.893 0 0 0 3.103-.563 8.854 8.854 0 0 0 3.09.563A8.903 8.903 0 0 0 24 12a8.904 8.904 0 0 0-8.903-8.903z" />
                 </svg>
               </a>
-              <span class="copy-popup">The link has been copied!</span>
+              <span className="copy-popup">The link has been copied!</span>
               <input
                 type="text"
-                value="https://nurui.fueko.net/im-passionate-about-food-the-tradition-of-it-cooking-it-sharing-it/"
+                defaultValue={`https://nurui.fueko.net/${slug}`}
                 id="link-value"
               />
             </div>
           </div>
         </div>
       </article>
-      <aside class="section-prev-next">
-        <div class="prev-next-wrap">
+      <aside className="section-prev-next">
+        <div className="prev-next-wrap">
           {prev && (
             <Link
               to={`/${kebabCase(prev.categories.name)}/${prev.slug}/`}
@@ -179,7 +178,7 @@ const BlogPost = ({ pageContext, data }) => {
             tag-story tag-hash-orange tag-hash-post-orange tag-hash-cta-violet
             no-image"
             >
-              <section class="prev-next-title">
+              <section className="prev-next-title">
                 <h5>Previous Post</h5>
                 <h3>{prev.title}</h3>
               </section>
