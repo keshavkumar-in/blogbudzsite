c13017bd35b48bc3051db84b9d96681fdac9e035
kkeshav414 <14369793+kkeshav414@users.noreply.github.com>
2020-05-03 05:28:32 +0530

    404 error page updated

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,12 +2,11 @@ import React from "react"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-
+import error from "../images/error.svg"
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <img src={error} alt="404: Not Found" />
   </Layout>
 )
 
