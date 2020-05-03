10c2c7da1c6bf1e8fd7d89d6df0ce3de6e9c009d
kkeshav414 <14369793+kkeshav414@users.noreply.github.com>
2020-05-04 03:29:14 +0530

    siteurl updated

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,7 @@ module.exports = {
     description: `An awesome blog displaying my awesome posts.`,
     author: `Blogbudz.com`,
     twitterHandle: `@geekykeshav`,
-    siteUrl: "https://blogbudz.com",
+    siteUrl: "https://blogbudz.com/",
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
