1d296df6e0497a5fe8179912e9906f69fcf88946
kkeshav414 <14369793+kkeshav414@users.noreply.github.com>
2020-05-07 08:19:37 +0530

    pwa updateready function chnged

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,11 +1,11 @@
 require("prismjs/themes/prism-okaidia.css")
 
-export const onServiceWorkerUpdateReady = () => {
-  const answer = window.confirm(
-    `This application has been updated. ` +
-      `Reload to display the latest version?`
-  )
-  if (answer === true) {
-    window.location.reload()
-  }
-}
+export const onServiceWorkerUpdateReady = () => window.location.reload()
+//   const answer = window.confirm(
+//     `This application has been updated. ` +
+//       `Reload to display the latest version?`
+//   )
+//   if (answer === true) {
+
+//   }
+// }
