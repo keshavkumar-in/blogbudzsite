89fcad5c54366358b0a997d70fdc5810970e9368
kkeshav414 <14369793+kkeshav414@users.noreply.github.com>
2020-05-07 08:21:55 +0530

    author page banner updated

--- src/pages/authors.js
+++ src/pages/authors.js
@@ -16,7 +16,6 @@ const AuthorPage = ({ data }) => {
           className="profile-wrap is-author"
           style={{
             backgroundImage: `url(${authoraHeader.file.url})`,
-            height: "50vh",
           }}
         >
           <h1>Authors</h1>
