51a3e35b8f8b7aa3a3584b8a600908bafa31f8ce
Blogbudz <blogbudz@gmail.com>
2020-05-18 16:38:58 +0530

    analytics code updated

--- gatsby-config.js
+++ gatsby-config.js
@@ -63,12 +63,12 @@ module.exports = {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         // The property ID; the tracking code won't be generated without it
-        trackingId: "UA-166951398-1",
+        trackingId: `UA-166951398-1`,
         // Defines where to place the tracking script - `true` in the head and `false` in the body
         head: true,
         // Defers execution of google analytics script after page load
         defer: true,
-        cookieDomain: "blogbudz.com",
+        cookieDomain: `blogbudz.com`,
       },
     },
     {
