371250a2fa366fd1d6b37963b525bbb40ffea009
Blogbudz <blogbudz@gmail.com>
2020-05-25 20:38:28 +0530

    google adsense code added

--- gatsby-config.js
+++ gatsby-config.js
@@ -200,6 +200,12 @@ module.exports = {
         ],
       },
     },
+    {
+      resolve: `gatsby-plugin-google-adsense`,
+      options: {
+        publisherId: `ca-pub-1988879979199899`,
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     `gatsby-plugin-offline`,
